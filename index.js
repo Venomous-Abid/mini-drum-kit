654df7c4f1221cd3ee92165634092c04a2848771
Abiduzzaman Abid <itisabidss@gmail.com>
2020-05-18 13:50:45 +0600

    Adding core files

---
+++ index.js
@@ -0,0 +1,82 @@
+
+//button activation
+for (var i = 0; i <=6; i++) {
+
+	document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+	
+	console.log(this.innerHTML);
+	var buttonText =this.innerHTML;
+
+	keycheckerFunction (buttonText);
+
+	buttonAnimation(buttonText);
+});
+
+}
+
+//button keystroke
+document.addEventListener("keypress", function(event){
+	keycheckerFunction (event.key);
+	buttonAnimation(event.key);
+});
+
+
+function keycheckerFunction (button) {
+
+			switch (button) {
+		case "w":
+			var tom1 = new Audio("sounds/tom-1.mp3");
+			tom1.play();
+			break;
+
+		case "a":
+			var tom2 = new Audio("sounds/tom-2.mp3");
+			tom2.play();
+			break;
+
+		case "s":
+			var tom3 = new Audio("sounds/tom-3.mp3");
+			tom3.play();
+			break;
+
+		case "d":
+			var tom4 = new Audio("sounds/tom-4.mp3");
+			tom4.play();
+			break;
+
+		case "j":
+			var crash = new Audio("sounds/crash.mp3");
+			crash.play();
+			break;
+
+		case "k":
+			var kb = new Audio("sounds/kick-bass.mp3");
+			kb.play();
+			break;
+
+		case "l":
+			var snare = new Audio("sounds/snare.mp3");
+			snare.play();
+			break;
+
+		default:
+			console.log(buttonText);
+			break;
+	}
+
+}
+
+
+function buttonAnimation(currentkey) {
+
+			var activeButton = document.querySelector("."+ currentkey);
+
+			activeButton.classList.add("pressed");
+
+			setTimeout(function(){
+				
+				activeButton.classList.remove("pressed");
+			
+			}, 100);
+
+}
